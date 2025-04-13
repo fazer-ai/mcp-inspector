debf91e92d89d17b42f7a9f95e8f3a58cc4c1370
gabrieljablonski <contact@gabrieljablonski.com>
2025-04-12 21:47:01 -0300

    feat: add env variables for UI constants

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MCP_SERVER_REQUEST_TIMEOUT: string;
+  readonly VITE_MCP_REQUEST_TIMEOUT_RESET_ON_PROGRESS: string;
+  readonly VITE_MCP_REQUEST_MAX_TOTAL_TIMEOUT: string;
+  readonly VITE_MCP_PROXY_FULL_ADDRESS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
